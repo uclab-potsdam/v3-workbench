8ae8bdaa62b2804f480f3358997b997ecec3eda0
Fidel Thomet <me@fidelthomet.com>
2021-06-08 10:09:42 +0200

    refactor: resolves #5

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,22 +1,44 @@
 <template>
   <div class="main">
     <TheCanvas/>
+    <div class="floating">
+      <TheSearchbar/>
+    </div>
   </div>
 </template>
 
 <script>
 import TheCanvas from '@/components/TheCanvas.vue'
+import TheSearchbar from '@/components/TheSearchbar.vue'
 
 export default {
   name: 'Main',
   components: {
-    TheCanvas
+    TheCanvas,
+    TheSearchbar
   }
 }
 </script>
 <style lang="scss" scoped>
 .main {
   position: relative;
-}
 
+  .floating {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 10;
+    pointer-events: none;
+    padding: var(--spacing);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    > * {
+      pointer-events: auto;
+    }
+  }
+}
 </style>

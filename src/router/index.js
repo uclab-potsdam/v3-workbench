3c4b12dbb03e59cb662f7805d472c55830e6a8a7
Fidel Thomet <git@ft0.ch>
2021-11-09 09:33:05 +0100

    chore: set router to hash mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
+// import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 // import Main from '../views/Main.vue'
 import store from '@/store'
 
@@ -25,7 +26,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 

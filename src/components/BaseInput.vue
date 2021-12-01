2183bec71c69130793c8d19e40602cb64ac18740
Giacomo Nanni <gn.nanni@gmail.com>
2021-12-01 14:15:06 +0100

    feat inputs and canvas button as component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="input">
+    <label class="input-prefix" v-if="prefix">{{ prefix }}</label>
+      <input
+        :type="type"
+        :placeholder="placeholder"
+        v-model="content"
+      />
+    <label class="input-suffix" v-if="suffix">{{ suffix }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseInput",
+  props: {
+    type: {
+      type: String,
+      default: "text",
+    },
+    placeholder: String,
+    label: String,
+    prefix: String,
+    suffix: String,
+    value: [Number, String]
+  },
+};
+</script>
+
+<style scoped lang="scss">
+input {
+    appearance: none;
+    height: calc(var(--spacing-xl));
+    background: var(--background);
+    box-sizing: border-box;
+    border: none;
+    color: var(--text);
+    font-weight: var(--bold);
+    border-bottom: var(--base-border);
+    font-size: var(--font-size-l);
+    font-weight: var(--bold);
+    padding: var(--spacing-s);
+
+    &:disabled {
+      color: var(--muted);
+      border-color: var(--muted);
+    }
+  }
+
+  label + input {
+    margin-top: var(--spacing-s);
+    margin-left: var(--spacing-s);
+  }
+
+  input + label {
+    margin-top: var(--spacing-s);
+    margin-left: var(--spacing-s);
+  }
+
+  input, button {
+    &:focus {
+      outline: 2px solid var(--accent);
+      outline-offset: 2px;
+    }
+  }
+
+  label.disabled {
+    color: var(--muted);
+  }
+</style>
---
+++ src/stories/BaseInput.stories.js
@@ -0,0 +1,49 @@
+import BaseInput from '@/components/BaseInput.vue'
+
+export default {
+  title: 'Components/Input',
+  component: BaseInput
+}
+
+const Template = ({ label, ...args }) => ({
+  components: { BaseInput },
+  setup () {
+    return { label, args }
+  },
+  template: '<BaseInput v-bind="args">{{label}}</BaseInput>'
+})
+
+export const Default = Template.bind({})
+Default.args = {
+  label: 'Default', 
+  type: 'text',
+  placeholder: 'Insert text',
+}
+
+export const Prefix = Template.bind({})
+Prefix.args = {
+  label: 'Prefix', 
+  type: 'text',
+  placeholder: 'Insert text',
+  prefix: 'prefix',
+}
+
+export const Suffix = Template.bind({})
+Suffix.args = {
+  label: 'Suffix', 
+  type: 'text',
+  placeholder: 'Insert text',
+  suffix: 'suffix',
+}
+
+export const Date = Template.bind({})
+Date.args = {
+  label: 'Default', 
+  type: 'date',
+}
+
+export const Password = Template.bind({})
+Password.args = {
+  label: 'Password', 
+  type: 'password',
+}
